5df99669947c85025b483842a5e9227e69aad87c
Rebecca <rebeccadavidsson3@gmail.com>
2018-11-05 13:47:49 +0100

    graph

---
+++ moviescraper.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# Name: Rebecca Davidsson
+# Student number: 11252138
+
+"""
+This script scrapes IMDB and outputs a CSV file with highest rated movies.
+"""
+
+import csv
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+import re
+
+TARGET_URL = "https://www.imdb.com/search/title?title_type=feature&release_date=2008-01-01,2018-01-01&num_votes=5000,&sort=user_rating,desc"
+BACKUP_HTML = 'movies.html'
+OUTPUT_CSV = 'movies.csv'
+
+
+def extract_movies(dom):
+    """
+    Extract a list of highest rated movies from DOM (of IMDB page).
+    Each movie entry should contain the following fields:
+    - Title
+    - Rating
+    - Year of release (only a number!)
+    - Actors/actresses (comma separated if more than one)
+    - Runtime (only a number!)
+    """
+
+    # Select relevant data
+    all_tables=dom.find_all('div', class_="lister-item mode-advanced")
+
+    movies = []
+
+    # Iterate over every row and extract relevant data
+    for row in all_tables:
+        title = row.find('h3').a.get_text()
+        rating = float(row.find('strong').get_text())
+        year = row.find('span', class_='lister-item-year text-muted unbold').get_text()
+        runtimes = row.find('span', class_='runtime').get_text()
+        actors = row.select('a[href*="adv_li_st_"]')
+
+        # Extract numbers from strings
+        years = [int(s) for s in re.findall(r'\d+', year)][0]
+        runtimes = [int(s) for s in re.findall(r'\d+', runtime)][0]
+
+        # Add every actor to a list of actor names
+        names = []
+        for actor in actors:
+            names.append(actor.get_text())
+
+        movies.append([title, rating, years, names, runtimes])
+
+    return movies
+
+
+def save_csv(outfile, movies):
+    """
+    Output a CSV file containing highest rated movies.
+    """
+    writer = csv.writer(outfile)
+    writer.writerow(['Title', 'Rating', 'Year', 'Actors', 'Runtime'])
+    for row in movies:
+        writer.writerow(row)
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    except RequestException as e:
+        print('The following error occurred during HTTP GET request to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns true if the response seems to be HTML, false otherwise
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+            and content_type is not None
+            and content_type.find('html') > -1)
+
+
+if __name__ == "__main__":
+
+    # get HTML content at target URL
+    html = simple_get(TARGET_URL)
+
+    # save a copy to disk in the current directory, this serves as an backup
+    # of the original HTML, will be used in grading.
+
+    with open(BACKUP_HTML, 'wb') as f:
+        f.write(html)
+
+    # parse the HTML file into a DOM representation
+    dom = BeautifulSoup(html, 'html.parser')
+
+    # extract the movies (using the function you implemented)
+    movies = extract_movies(dom)
+
+    # write the CSV file to disk (including a header)
+    with open(OUTPUT_CSV, 'w', newline='') as output_file:
+        save_csv(output_file, movies)
---
+++ visualizer.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# Name: Rebecca Davidsson
+# Student number: 11252138
+"""
+This script visualizes data obtained from a .csv file
+"""
+
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Global constants for the input file, first and last year
+INPUT_CSV = 'movies.csv'
+START_YEAR = 2008
+END_YEAR = 2018
+
+
+def get_mean(key):
+    """
+    Calculates mean rating for each year.
+    """
+
+    # Start reading from beginning of file for each key
+    with open(INPUT_CSV) as movies:
+        movies = csv.reader(movies, delimiter=',')
+        next(movies)
+
+        ratings = []
+
+        # Calculate mean rating of every year
+        for row in movies:
+            if key == int(row[2]):
+                ratings.append(float(row[1]))
+        rating = round((sum(ratings)/len(ratings)),3)
+
+    return rating
+
+def plot(dict):
+    """
+    Plots dictionary into a graph
+    """
+
+    plt.ylabel('Average rating', fontsize = 18)
+    plt.xlabel('Year', fontsize = 18)
+    plt.ylim(8, 9.4)
+
+    # Plot a line and bar in the same plot
+    plt.plot(dict.keys(), dict.values(), color="red")
+    plt.bar(dict.keys(), dict.values())
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Global dictionary for the data
+    data_dict = {str(key): get_mean(key) for key in range(START_YEAR, END_YEAR)}
+
+    print(data_dict)
+
+    plot(data_dict)
