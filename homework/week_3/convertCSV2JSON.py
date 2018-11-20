3933acf42ae177c5a775afcf36c3b01e71cf9171
Rebecca <rebeccadavidsson3@gmail.com>
2018-11-20 08:32:34 +0100

    tijdelijk opslaan

---
+++ homework/week_3/convertCSV2JSON.py
@@ -0,0 +1,20 @@
+import csv
+import json
+
+# Read the csv file
+dates = []
+sun = []
+with open('sun.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        dates.append(int(row[1]))
+        sun.append(int(row[2].strip(" ")))
+
+# Make a dictionary of the data
+dict = {}
+for i in range(len(dates)):
+    dict.update({i: sun[i]})
+
+# Put the dictionary into a json file
+with open('sun.json', 'w') as outfile:
+    json.dump(dict, outfile)
