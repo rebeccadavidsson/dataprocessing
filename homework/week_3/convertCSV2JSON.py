b61832500e4b861cc24ccea0b9d15f75773c2705
Rebecca <rebeccadavidsson3@gmail.com>
2018-11-21 12:11:50 +0100

    data

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -7,13 +7,13 @@ sun = []
 with open('sun.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-        dates.append(int(row[1]))
+        dates.append(row[1])
         sun.append(int(row[2].strip(" ")))
 
 # Make a dictionary of the data
 dict = {}
 for i in range(len(dates)):
-    dict.update({i: sun[i]})
+    dict.update({dates[i]: sun[i]})
 
 # Put the dictionary into a json file
 with open('sun.json', 'w') as outfile:
