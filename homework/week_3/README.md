5f34d4a13bc0f922447da09655b99ed2f3ce57ce
Rebecca <rebeccadavidsson3@gmail.com>
2018-11-21 15:21:26 +0100

    final

---
+++ homework/week_3/README.md
@@ -0,0 +1,19 @@
+# Data Line Chart
+
+Name: Rebecca Davidsson
+Student number: 11252138
+
+## Structure
+
+The website can be opened by running a local server on:
+
+```
+python -m http.server 8080
+```
+
+and open 'index.html'.
+
+## Data
+
+Two data files (named sun.csv and sun2.csv) are converted by convertCSVJSON.py.
+Output of convertCSVJSON.py is data.json, which is used by index.html. 
--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -1,20 +1,41 @@
+"""
+Name: Rebecca Davidsson
+Student number: 11252138
+convertCSVJSON.py:
+Converts two csv files and merges the files into one dictionary.
+Input csv files include data per day (starting from day 1).
+The dictionary is converted into one json file.
+"""
+
 import csv
 import json
 
 # Read the csv file
 dates = []
 sun = []
-with open('sun.csv', 'r') as csvfile:
+sun2 = []
+counter = 1
+input_first_file = 'sun.csv'
+input_second_file = 'sun2.csv'
+
+with open(input_first_file, 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-        dates.append(row[1])
+        dates.append(counter)
         sun.append(int(row[2].strip(" ")))
+        counter += 1
+
+with open(input_second_file, 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        sun2.append(int(row[2].strip(" ")))
 
 # Make a dictionary of the data
 dict = {}
-for i in range(len(dates)):
-    dict.update({dates[i]: sun[i]})
+for i in range(len(sun)):
+    print({dates[i]: {"oktober": sun[i], "november": sun2[i]}})
+    dict.update({dates[i]: {"oktober": sun[i], "november": sun2[i]}})
 
 # Put the dictionary into a json file
-with open('sun.json', 'w') as outfile:
+with open('data.json', 'w') as outfile:
     json.dump(dict, outfile)
