dbeb4458254a8c2b37760b8e92083cbd76d3de96
Rebecca <rebeccadavidsson3@gmail.com>
2018-11-30 11:40:41 +0100

    Versie1

--- homework/week_5/scatter.js
+++ homework/week_5/scatter.js
@@ -19,7 +19,9 @@ Promise.all(requests).then(function(response) {
     .append("p")
     .text("Name: Rebecca Davidsson, student number: 11252138, Date: 3 december 2018")
     .append("p")
-    .text("Data sourse: http://stats.oecd.org/ ");
+    .text("Data sourse: http://stats.oecd.org/ ")
+    .append("p")
+    .text("Click on any country name to view the scatterplot. ");
 
   // Start with data of France when html is opened
   var country = "France"
@@ -114,7 +116,7 @@ Promise.all(requests).then(function(response) {
         .attr("y", padding - 10)
         .attr("text-anchor", "middle")
         .attr("class", "title")
-        .text("Correlation between number of female researchers and consumer confidence over the years ...");
+        .text("Correlation between number of female researchers and consumer confidence");
 
   // again scaleOrdinal
 	const colorScale = d3.scaleOrdinal()
@@ -168,7 +170,17 @@ Promise.all(requests).then(function(response) {
        .on("click",function(d,i) { updateData(countryArray[i]);});
 
 
+
    function updateData(country) {
+
+     d3.select("svg").remove();
+
+     // Get the years of each datapoint
+     yearsArray = []
+     for(var i=0; i<9; i++)  {
+       yearsArray.push(dataset0[i]["time"])
+     }
+
 
      // Make an empty array and push all datapoints
      array0 = []
@@ -186,7 +198,7 @@ Promise.all(requests).then(function(response) {
        }
      }
 
-     // Make an array of a combined datapoints (of both json files)
+     // Make an array of a combined datapoint (of both json files)
      combinedArray = []
      for(var i=0; i<9; i++)  {
        // Filter out empty datapoints
@@ -195,51 +207,107 @@ Promise.all(requests).then(function(response) {
        }
      }
 
-   // COMMENT
-   var xScale = d3.scaleLinear()
-       .domain([Math.min.apply(Math, array0) - 0.5, Math.max.apply(Math, array0) + 0.5])
-       .range([padding, w - 300]);
+     // Create new SVG element
+     var svg = d3.select("body")
+         .append("svg")
+         .attr("width", w)
+         .attr("height", h)
 
-   // COMMENT
-   var yScale = d3.scaleLinear()
-       .domain([Math.min.apply(Math, array1) - 1, Math.max.apply(Math, array1) + 1])
-       .range([h - padding, padding]);
+     // COMMENT
+     var xScale = d3.scaleLinear()
+         .domain([Math.min.apply(Math, array0) - 0.5, Math.max.apply(Math, array0) + 0.5])
+         .range([padding, w - 300]);
 
-   // Make an x-axis by appending a group element
-   svg.append("g")
-     .attr("class", "x axis")
-     .attr("transform", "translate(0," + (h - padding) + ")")
-     .call(d3.axisBottom(xScale));
+     // COMMENT
+     var yScale = d3.scaleLinear()
+         .domain([Math.min.apply(Math, array1) - 1, Math.max.apply(Math, array1) + 1])
+         .range([h - padding, padding]);
 
-   // Make a y-axis by appending a group element
-   svg.append("g")
-     .attr("class", "y axis")
-     .attr("transform", "translate(50,0)")
-     .call(d3.axisLeft(yScale));
+     // Make an x-axis by appending a group element
+     svg.append("g")
+       .attr("class", "x axis")
+       .attr("transform", "translate(0," + (h - padding) + ")")
+       .call(d3.axisBottom(xScale));
 
+     // Make a y-axis by appending a group element
+     svg.append("g")
+       .attr("class", "y axis")
+       .attr("transform", "translate(50,0)")
+       .call(d3.axisLeft(yScale));
 
-  svg.selectAll("circle")
-      .exit()
-      .remove();
+     // text label for the y axis
+     svg.append("text")
+         .attr("class", "text")
+         .attr("transform","rotate(-90,60,50)")
+         .style("text-anchor", "middle")
+         .text("Consumer confidence");
 
-   // Create Circles
-   svg.selectAll("circle")
-       .data(combinedArray)
-       .enter()
-       .append("circle")
-       .attr("cx", function(d) {
-           return xScale(d[0]);
-       })
-       .attr("cy", function(d) {
-           return yScale(d[1]);
-       })
-       .attr("r", 5)
-       .style("fill", function(d) { return colorScale(d);})
+     // text label for the x-axis
+     svg.append("text")
+         .attr("class", "text")
+         .attr("transform","translate(400,390)")
+         .style("text-anchor", "middle")
+         .text("Women researchers as a percentage of total researchers (headcount)")
 
-    console.log(country)
+     // add title to the scatterplot
+     svg.append("text")
+           .attr("x", h)
+           .attr("y", padding - 10)
+           .attr("text-anchor", "middle")
+           .attr("class", "title")
+           .text("Correlation between number of female researchers and consumer confidence");
+
+     // again scaleOrdinal
+   	const colorScale = d3.scaleOrdinal()
+         .range(d3.schemePaired);
+
+     // Create Circles
+     svg.selectAll("circle")
+         .data(combinedArray)
+         .enter()
+         .append("circle")
+         .attr("cx", function(d) {
+             return xScale(d[0]);
+         })
+         .attr("cy", function(d) {
+             return yScale(d[1]);
+         })
+         .attr("r", 5)
+         .style("fill", function(d) { return colorScale(d);})
+
+     // construct legend
+     var legend = svg.selectAll(".legend")
+          .data(colorScale.domain())
+          .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d, i)
+          { return "translate(0," + i * 30 + ")"; });
+
+     // create rectangles for the colors of the legend
+     legend.append("rect")
+          .attr("x", w - legendHeight)
+          .attr("width", legendHeight)
+          .attr("height", legendHeight)
+          .style("fill", colorScale);
+
+     // add text to the legend
+     legend.append("text")
+          .attr("x", w - 24)
+          .attr("y", 9)
+          .attr("dy", 8)
+          .style("text-anchor", "end")
+          .text(function(d, i) { return yearsArray[i]; });
+
+     // add country links
+     legend.append("text")
+          .attr("x", w - 150)
+          .attr("y", 9)
+          .attr("dy", 8)
+          .attr("class", "links")
+          .style("text-anchor", "end")
+          .text(function(d, i) { return countryArray[i]; })
+          .on("click",function(d,i) { updateData(countryArray[i]);});
    }
-
-
 
 
 }).catch(function(e){
